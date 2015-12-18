29e01dd03bf04ebcd143953f5e249b4f19bd4eea
Stevie Strickland <sstrickl@racket-lang.org>
2015-12-18 11:27:18 -0500

    Change this so it is strictly exits.

--- core_shim.ino
+++ core_shim.ino
@@ -381,9 +381,9 @@ void loop() {
             delay(1000);
           }
         } else {
-          if(!(curr->old_value <= curr->min_value ||
-               curr->old_value >= curr->max_value) &&
-             (curr_val <= curr->min_value || curr_val >= curr->max_value)) {
+          if(!(curr->old_value < curr->min_value ||
+               curr->old_value > curr->max_value) &&
+             (curr_val < curr->min_value || curr_val > curr->max_value)) {
             char s[10];
             sprintf(s, "%d", curr_val);
             Spark.publish(curr->event, s, 60, PRIVATE);
