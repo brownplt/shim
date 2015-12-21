c03f45e8b8cc63d65a18e6a981ed24e2869bcacd
Stevie Strickland <sstrickl@racket-lang.org>
2015-12-21 14:41:33 -0500

    Cleanups, also making generic read/write functions.
    The generic functions follow the same analog/digital restrictions as the
    config.

--- core_shim.ino
+++ core_shim.ino
@@ -1,7 +1,5 @@
-int tinkerDigitalRead(String pin);
-int tinkerDigitalWrite(String command);
-int tinkerAnalogRead(String pin);
-int tinkerAnalogWrite(String command);
+int tinkerRead(String);
+int tinkerWrite(String);
 
 int string_to_pin(const char *str) {
   switch(str[0]) {
@@ -309,11 +307,11 @@ void configure(const char *event, const char *edata) {
 }
 
 void dump(const char *event, const char *edata) {
-  emit_dump("core_dump", config);
+  emit_dump("config_dump", config);
 }
 
 int ext_dump(String arg) {
-  emit_dump("core_dump", config);
+  emit_dump("config_dump", config);
   return 0;
 }
 
@@ -415,17 +413,6 @@ void name_handler(const char *event, const char *data) {
   Spark.subscribe(s, dump, MY_DEVICES);
 }
 
-void setup() {
-  Spark.function("digitalread", tinkerDigitalRead);
-  //Spark.function("digitalwrite", tinkerDigitalWrite);
-  Spark.function("analogread", tinkerAnalogRead);
-  //Spark.function("analogwrite", tinkerAnalogWrite);
-  Spark.function("dump", ext_dump);
-  Spark.function("dump_caches", dump_caches);
-  Spark.subscribe("spark/", name_handler);
-  Spark.publish("spark/device/name", NULL, 60, PRIVATE);
-}
-
 int dump_caches(String data) {
  char s[255];
  int i, printed = 0;
@@ -441,6 +428,15 @@ int dump_caches(String data) {
  delay(1000);
  Spark.publish("cache_dump", s, 60, PRIVATE);
  return 0;
+}
+
+void setup() {
+  Spark.function("read", tinkerRead);
+  Spark.function("write", tinkerWrite);
+  Spark.function("dump_config", ext_dump);
+  Spark.function("dump_caches", dump_caches);
+  Spark.subscribe("spark/", name_handler);
+  Spark.publish("spark/device/name", NULL, 60, PRIVATE);
 }
 
 void loop() {
@@ -502,122 +498,61 @@ void loop() {
   if(published) { delay(1000); }
 }
 
-// The following is taken directly from Particle's Tinker app.
+// The following is taken with changes from Particle's Tinker app.
 /*******************************************************************************
- * Function Name  : tinkerDigitalRead
- * Description    : Reads the digital value of a given pin
+ * Function Name  : tinkerRead
+ * Description    : Reads the value of a given pin
  * Input          : Pin
  * Output         : None.
- * Return         : Value of the pin (0 or 1) in INT type
+ * Return         : Value of the pin in INT type
                     Returns a negative number on failure
  *******************************************************************************/
-int tinkerDigitalRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+int tinkerRead(String cmd) {
+        const char *command = cmd.c_str();
+        int pin = string_to_pin(command);
+        if(pin < 0) { return -1; }
 
-	if(pin.startsWith("D"))
-	{
-		pinMode(pinNumber, INPUT_PULLDOWN);
-		return digitalRead(pinNumber);
-	}
-	else if (pin.startsWith("A"))
-	{
-		pinMode(pinNumber+10, INPUT_PULLDOWN);
-		return digitalRead(pinNumber+10);
-	}
-	return -2;
+        pinMode(pin, INPUT_PULLDOWN);
+        if(is_analog_input(pin)) {
+          return analogRead(pin);
+        } else {
+          return digitalRead(pin);
+        }
 }
 
 /*******************************************************************************
- * Function Name  : tinkerDigitalWrite
- * Description    : Sets the specified pin HIGH or LOW
+ * Function Name  : tinkerWrite
+ * Description    : Sets the specified pin HIGH or LOW if digital,
+ *                  or to numeric value if analog
  * Input          : Pin and value
  * Output         : None.
  * Return         : 1 on success and a negative number on failure
  *******************************************************************************/
-int tinkerDigitalWrite(String command)
+int tinkerWrite(String cmd)
 {
-	bool value = 0;
+        const char *command = cmd.c_str();
+	int value = 0;
 	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
+	int pinNumber = string_to_pin(command);
 	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
+	if (pinNumber < 0) return -1;
 
-	if(command.substring(3,7) == "HIGH") value = 1;
-	else if(command.substring(3,6) == "LOW") value = 0;
-	else return -2;
+	const char *valstr = command + 3;
 
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		digitalWrite(pinNumber, value);
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		digitalWrite(pinNumber+10, value);
-		return 1;
-	}
-	else return -3;
+        if(is_analog_output(pinNumber)) {
+          if(sscanf(valstr, "%d", &value) <= 0 || value < 0 || value > 4095) {
+            return -2;
+          }
+          pinMode(pinNumber, OUTPUT);
+          analogWrite(pinNumber, value);
+        } else {
+          if(!strncmp(valstr, "HIGH", 4)) value = 1;
+	  else if(!strncmp(valstr, "LOW", 3)) value = 0;
+	  else return -2;
+          pinMode(pinNumber, OUTPUT);
+          digitalWrite(pinNumber, value);
+        }
+        return 1;
+
 }
 
-/*******************************************************************************
- * Function Name  : tinkerAnalogRead
- * Description    : Reads the analog value of a pin
- * Input          : Pin
- * Output         : None.
- * Return         : Returns the analog value in INT type (0 to 4095)
-                    Returns a negative number on failure
- *******************************************************************************/
-int tinkerAnalogRead(String pin)
-{
-	//convert ascii to integer
-	int pinNumber = pin.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	if(pin.startsWith("D"))
-	{
-		return -3;
-	}
-	else if (pin.startsWith("A"))
-	{
-		return analogRead(pinNumber+10);
-	}
-	return -2;
-}
-
-/*******************************************************************************
- * Function Name  : tinkerAnalogWrite
- * Description    : Writes an analog value (PWM) to the specified pin
- * Input          : Pin and Value (0 to 255)
- * Output         : None.
- * Return         : 1 on success and a negative number on failure
- *******************************************************************************/
-int tinkerAnalogWrite(String command)
-{
-	//convert ascii to integer
-	int pinNumber = command.charAt(1) - '0';
-	//Sanity check to see if the pin numbers are within limits
-	if (pinNumber< 0 || pinNumber >7) return -1;
-
-	String value = command.substring(3);
-
-	if(command.startsWith("D"))
-	{
-		pinMode(pinNumber, OUTPUT);
-		analogWrite(pinNumber, value.toInt());
-		return 1;
-	}
-	else if(command.startsWith("A"))
-	{
-		pinMode(pinNumber+10, OUTPUT);
-		analogWrite(pinNumber+10, value.toInt());
-		return 1;
-	}
-	else return -2;
-}
